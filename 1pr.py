e72c73fe4eea9bc0c6301e0a83bc516e988182b2
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 16:43:43 +0300

    Update 1pr.py

--- 1pr.py
+++ 1pr.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
-  def add_four(a):
-  x = 2
+    def add_four(a):
+        x = 2
 
 
-  def add_some():
-  print("x = " + str(x))
-  return a + x
-  return add_some()
+        def add_some():
+            print("x = " + str(x))
+            return a + x
+        return add_some()
 
-  print(add_four(5))
+    print(add_four(5))
