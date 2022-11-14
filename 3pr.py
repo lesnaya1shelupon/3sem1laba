d39583f2ef14bd9799f0796b1deefe0d11c7b509
lesnaya1shelupon <102218456+lesnaya1shelupon@users.noreply.github.com>
2022-11-14 16:38:59 +0300

    Update 3pr.py

--- 3pr.py
+++ 3pr.py
@@ -1,9 +1,14 @@
-def fun1(a):
-    x = a * 3
-    def fun2(b):
-        nonlocal x
-        return b + x
-    return fun2
 if __name__ == "__main__":
+
+
+    def fun1(a):
+    x = a * 3
+
+
+        def fun2(b):
+            nonlocal x
+            return b + x
+        return fun2
+
     test_fun = fun1(4)
     print(test_fun(7))
